c09d7f60cf72b7aedede671616b3dea69e4420f2
eduardo <106855879+eduardodfran@users.noreply.github.com>
2025-11-22 08:29:12 +0800

    Refactor user signup process to streamline user data handling and ensure unique user IDs

--- backend/main.py
+++ backend/main.py
@@ -32,19 +32,16 @@ async def signup(user: User):
     hash_object = hashlib.sha256()
     hash_object.update(user.password.encode())
     user.password = hash_object.hexdigest()
-    user_dict = user.model_dump()
-    print(user_dict)
-    data = []
+    raw_dict = user.model_dump()
     with open("./data/users.json", "r") as f:
         data = json.load(f)
-    data["users"].append(user_dict)
-    
-    # auto id
     new_id = len(data["users"]) + 1
-    user_dict["id"] = new_id
-
-    # append
-    data["users"].append(user_dict)
-    
+    user_dict = {
+        "id": new_id,
+        **raw_dict
+    }
+    data["users"].append(user_dict)   
     with open("./data/users.json", "w") as f:
         json.dump(data, f, indent=4)
+        
+    return {"message": "User created successfully"}
